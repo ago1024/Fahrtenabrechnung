a8f706b7302bf73ca84f7f864c0aa7df60450167
Alexander Gottwald <alexander.gottwald@gmail.com>
2024-11-17 20:50:08 +0100

    added ngx-env builder

--- src/app/services/maps.service.ts
+++ src/app/services/maps.service.ts
@@ -4,20 +4,13 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class MapsService {
 
-  constructor() {}
-
   getUrl(from: Location, to: Location): string {
     const url = new URL('https://www.google.com/maps/embed/v1/directions');
-    url.searchParams.append('key', 'AIzaSyDWcY1BoJZ8zKDdZPoN7g33b93BIHETQIQ');
+    url.searchParams.append('key', import.meta.env.NG_APP_MAPS_APIKEY || 'MAPS_APIKEY_DUMMY');
     url.searchParams.append('origin', from.address);
     url.searchParams.append('destination', to.address);
     url.searchParams.append('mode', 'driving');
     return url.toString();
   }
 
-  openMaps(from: Location, to: Location) {
-    const url = this.getUrl(from, to);
-    console.log(url);
-    window.open(url);
-  }
 }
